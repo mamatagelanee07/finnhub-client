4e87cd5333c31e4f93cb57971191db15c11f2207
Mamata Gelanee <gelaneem@gmail.com>
2026-03-03 10:41:44 +0000

    Add : domain module with models

---
+++ domain/src/main/java/com/andigeeky/finnhub/domain/GetUpcomingIPOCalendarUseCase.kt
@@ -0,0 +1,10 @@
+package com.andigeeky.finnhub.domain
+
+import com.andigeeky.finnhub.domain.common.Resource
+import com.andigeeky.finnhub.domain.models.IPOCalendar
+import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
+
+interface GetUpcomingIPOCalendarUseCase {
+    suspend operator fun invoke(from: LocalDate, to: LocalDate): Flow<Resource<IPOCalendar>>
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,4 +24,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "finnhub-client"
 include(":app")
- 
+include(":domain")
